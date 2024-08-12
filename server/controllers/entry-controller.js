a6c9a05b80e876a45293279a6b0c892dd78a1d79
Emily Jarecki <jareckiemily@yahoo.com>
2024-08-12 09:07:51 -0500

    rest of routes working

--- server/controllers/entry-controller.js
+++ server/controllers/entry-controller.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { handleValidateOwnership, requireToken } = require("../middleware/auth");
 const { Entry } = require("../models/lib");
 
+// get all working
 router.get("/", async (req, res) => {
   try {
     const entry = await Entry.find({})
@@ -15,6 +16,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+// working
 router.post("/", requireToken, async (req, res, next) => {
   try {
     const owner = req.user._id;
@@ -28,6 +30,7 @@ router.post("/", requireToken, async (req, res, next) => {
   }
 });
 
+// working
 router.put("/:id", requireToken, async (req, res) => {
   try {
     handleValidateOwnership(req, await Entry.findById(req.params.id));
@@ -41,4 +44,54 @@ router.put("/:id", requireToken, async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+// get all posts relative towards user
+// working
+router.get("/all", requireToken, async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const post = await Entry.find({ owner: userId })
+      .populate("owner", "username -_id")
+      .exec();
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the entries." });
+  }
+});
+
+// delete a single post
+router.delete("/:id", requireToken, async (req, res, next) => {
+  try {
+    handleValidateOwnership(req, await Entry.findById(req.params.id));
+    const deletedEntry = await Entry.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedEntry);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// get all user posts from a single category
+router.get("/:categoryId", requireToken, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const entry = await Entry.find({
+      owner: userId,
+      category_id: req.params.categoryId,
+    })
+      .populate("owner", "username -_id")
+      .exec();
+    console.log(entry);
+    res.status(200).json(entry);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the entries." });
+  }
+});
+
 module.exports = router;
