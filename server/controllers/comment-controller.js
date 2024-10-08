e619d413114c6229a18407cd6b246fe10658affc
Emily Jarecki <jareckiemily@yahoo.com>
2024-10-08 14:58:36 -0500

    able to delete individual comment. view comments relative to single entry

--- server/controllers/comment-controller.js
+++ server/controllers/comment-controller.js
@@ -33,15 +33,49 @@ router.post("/:entryId", requireToken, async (req, res, next) => {
 });
 
 // cant get this to work :(
-router.delete("/delete/:id", requireToken, async (req, res, next) => {
+// router.delete("/all", requireToken, async (req, res, next) => {
+//     try {
+//         handleValidateOwnership(req, await Comment.findById(req.params.id));
+//         const deletedComment = await Comment.findByIdAndDelete(req.params.id);
+//         console.log("deleted comment: ", deletedComment)
+//         res.status(200).json(deletedComment);
+//     } catch (err) {
+//       res.status(400).json({ error: err.message });
+//     }
+//   });
+
+router.delete("/:entryId/comments/:commentId", requireToken, async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    if (comment.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Unauthorized to delete this comment" });
+    }
+    const deletedComment = await Comment.findByIdAndDelete(req.params.commentId);
+    console.log("deleted comment: ", comment)
+    res.status(200).json(deletedComment);
+
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+  router.get("/:entryId/all", requireToken, async (req, res) => {
     try {
-        handleValidateOwnership(req, await Comment.findById(req.params.id));
-        const deletedComment = await Comment.findByIdAndDelete(req.params.id);
-        console.log("deleted comment: ", deletedComment)
-        res.status(200).json(deletedComment);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
+      const entry = await Entry.findById(req.params.entryId).populate("comments");
+  
+      if (!entry) {
+        return res.status(404).json({ message: "Entry not found" });
+      }
+  
+      return res.status(200).json({ comments: entry.comments });
+    } catch (error) {
+      return next(error);
     }
   });
+
 
 module.exports = router;
